ef6818ca2ab6893ab0397282437ebe09b273f8bf
陈张萌 <chenzm17@mails.tsinghua.edu.cn>
2021-04-08 21:37:14 +0800

    [init]....ing

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,30 @@ pub struct VcpuState {
     pub rflags: u64,
 }
 
+#[cfg(target_arch = "riscv64")]
+#[repr(C)]
+#[derive(Debug, Default)]
+pub struct VcpuState {
+    pub rax: u64,
+    pub rcx: u64,
+    pub rdx: u64,
+    pub rbx: u64,
+    pub rsp: u64,
+    pub rbp: u64,
+    pub rsi: u64,
+    pub rdi: u64,
+    pub r8: u64,
+    pub r9: u64,
+    pub r10: u64,
+    pub r11: u64,
+    pub r12: u64,
+    pub r13: u64,
+    pub r14: u64,
+    pub r15: u64,
+    // Contains only the user-controllable lower 32-bits.
+    pub rflags: u64,
+}
+
 #[cfg(target_arch = "aarch64")]
 #[repr(C)]
 #[derive(Debug, Default)]
